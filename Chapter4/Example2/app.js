ae5cfbeec594b57836ac000f62b4fedba4971e9a
roshanKadasani <roshan_123@csu.fullerton.edu>
2016-09-26 17:58:51 -0700

    formating changes

--- Chapter4/Example2/app.js
+++ Chapter4/Example2/app.js
@@ -1,44 +1,46 @@
+/* jshint browser: true, jquery: true, camelcase: true, indent: 2, undef: true, quotmark: single, maxlen: 80, trailing: true, curly: true, eqeqeq: true, forin: true, immed: true, latedef: true, newcap: true, nonew: true, unused: true, strict: true */
+
 var main = function () {
-    "use strict";
+    'use strict';
 
     var addCommentFromInputBox = function () {
-        var $new_comment;
+        var $newComment;
 
-        if ($(".comment-input-1 input").val() !== "") {
-            $new_comment = $("<p>").text($(".comment-input-1 input").val());
-            $new_comment.hide();
-            $(".comments").append($new_comment);
-            $new_comment.fadeIn();
-            $(".comment-input-1 input").val("");
+        if ($('.comment-input-1 input').val() !== '') {
+            $newComment = $('<p>').text($('.comment-input-1 input').val());
+            $newComment.hide();
+            $('.comments').append($newComment);
+            $newComment.fadeIn();
+            $('.comment-input-1 input').val('');
         }
     };
     var addCommentFromInputBox2 = function () {
-        var $new_comment;
+        var $newComment;
 
-        if ($(".comment-input-2 input").val() !== "") {
-            $new_comment = $("<p>").text($(".comment-input-2 input").val());
-            $new_comment.hide();
-            $(".comments").append($new_comment);
-            $new_comment.fadeIn();
-            $(".comment-input-2 input").val("");
+        if ($('.comment-input-2 input').val() !== '') {
+            $newComment = $('<p>').text($('.comment-input-2 input').val());
+            $newComment.hide();
+            $('.comments').append($newComment);
+            $newComment.fadeIn();
+            $('.comment-input-2 input').val('');
         }
     };
 
-    $(".comment-input-1 button").on("click", function (event) {
+    $('.comment-input-1 button').on('click', function (event) {
         addCommentFromInputBox();
     });
 
-    $(".comment-input-1 input").on("keypress", function (event) {
+    $('.comment-input-1 input').on('keypress', function (event) {
         if (event.keyCode === 13) {
             addCommentFromInputBox();
         }
     });
 
-    $(".comment-input-2 button").on("click", function (event) {
+    $('.comment-input-2 button').on('click', function (event) {
         addCommentFromInputBox2();
     });
 
-    $(".comment-input-2 input").on("keypress", function (event) {
+    $('.comment-input-2 input').on('keypress', function (event) {
         if (event.keyCode === 13) {
             addCommentFromInputBox2();
         }
